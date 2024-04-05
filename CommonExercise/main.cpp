3dcdfc7306d0912592a801e5d3cf453571e64c89
Venislav84 <133969005+Venislav84@users.noreply.github.com>
2024-04-05 13:47:11 +0300

    updated solution
    updated solution

--- CommonExercise/main.cpp
+++ CommonExercise/main.cpp
@@ -2,6 +2,9 @@
 #include <QLocale>
 #include <QTranslator>
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
 // Task. 1  Да се напише програма, която по въведени от клавиатурата цели числа x и p намира и извежда на екрана p-тата цифра на x.
@@ -61,12 +64,90 @@ using namespace std;
 // съществува число, чиито цифри изпълняват условието: ц1 .... цn-3 < цn-2 > цn-1 < цn,
 // където знаците се сменят алтернативно. Например: 1324, 1 < 3 > 2 < 4.
 
+// int countDigits(int num) {
+//     int count = 0;
+//     while (num != 0) {
+//         num = num / 10;
+//         count++;
+//     }
+//     return count;
+// }
 
+// int compareDigits(int num1) {
+//     int prevDigit = num1 % 10;
+//     num1 /= 10;
+
+//     while (num1 > 0) {
+//         int currentDigit = num1 % 10;
+
+//         if (currentDigit < prevDigit) {
+//             std::cout << currentDigit << " < " << prevDigit << " " ;
+
+//         }
+
+//         else if (currentDigit > prevDigit) {
+//             std::cout << currentDigit << " > " << prevDigit << " " ;
+//         }
+
+//         prevDigit = currentDigit;
+//         num1 /= 10;
+//     }
+//     std::cout << std::endl;
+// }
+
+// int main() {
+
+//     int p, q; std::cout << "Enter two positive integers p and q: "; std::cin >> p >> q;
+
+//     int numDigits1 = countDigits(p);
+//     int numDigits2 = countDigits(q);
+//     cout << "Number of digits in p: " << numDigits1 << endl;
+//     cout << "Number of digits in q: " << numDigits2 << endl;
+
+//     for (int i = p; i <= q; i++) {
+//         compareDigits(i);
+//     }
+
+//     return 0;
+// }
 
 // -------------------------------------------------------------------------------------------------------
 
 // Task. 4 Да се напише функция, която по зададено цяло число n, n > 0, намира fib(n).
 // Редицата от числата на Фибоначи: 1, 1, 2, 3, 5, 8, 13, 21...
+
+//  ----------------   with for loop   -------------------------
+
+// int main (){
+
+//     int num;
+//     int num1 = 0;
+//     int num2 = 1;
+//     int nextNums = 0;
+//     cout << "Enter the number of terms: ";
+//     cin >> num;
+//     cout << "Fibonatchi sequence: ";
+
+//     for (int i = 1; i <= num; ++i) {
+//         if(i == 1){
+//             cout << num1 << ", ";
+//             continue;
+//         }
+//         if(i == 2){
+//             cout << num2 << ", ";
+//             continue;
+//         }
+//         nextNums = num1 + num2;
+//         num1 = num2;
+//         num2 = nextNums;
+
+//         cout << nextNums << ", ";
+//     }
+
+//     return 0;
+// }
+
+//  ----------------   with recursion   -------------------------
 
 // int fib(int n)
 // {
@@ -177,13 +258,162 @@ using namespace std;
 // който е равен на средното аритметично от неговите съседни елементи (крайните елементи имат само по един съседен).
 // Ако такъв елемент не съществува, да се връща -1.
 
+// int findIndex(std::vector<double> vec) {
+//     for (int i = vec.size() - 1; i >= 0; i--) {
+//         if(i == vec.size() - 1){
+//             if(vec.at(i) == (vec.at(i - 1)/ 2)) {
+
+//                 return i;
+//             }
+//         } else if(i == 0){
+//             if(vec.at(i) == (vec.at(i + 1)/ 2)){
+
+//                 return i;
+//             }
+//         // if (vec.size() == 2 && vec.at(i) == (vec.at(i + 1)/ 2)) {
+//         //     return i;
+//         // }
+//         // if (vec.size() == 2 && vec.at(i) == (vec.at(i - 1)/ 2)) {
+//         //     return i;
+//         // }
+//         } else
+//         if (vec.at(i) == ((vec.at(i-1) + vec.at(i+1)) / 2)) {
+
+//             return i;
+//         }
+
+//         }
+//     return -1;
+// }
+
+// int main() {
+
+//     std::vector<double> vec = {4.0, 2.0, 8.0, 6.0, 4.0, 3.0};
+
+//     int index = findIndex(vec);
+
+//     if (index != -1) {
+//         std::cout << "Index of last element equal to the average of its neighbors: " << index << std::endl;
+//     } else {
+//         std::cout << "No such element exists in the vector." << std::endl;
+//     }
+
+//     return 0;
+// }
+
+
+// -------------------------------------------------------------------------------------------------------
+
+// Task. 8 Да се дефинира функция, която приема две редици и извежда тяхното сечение, сортирано във възходящ ред.
+
+// дефинирам функция intersection, която приема два масива от цели числа arr1 и arr2
+// и извежда техните общи елементи (сечение) сортирани във възходящ ред.
+// Функцията използва алгоритъма std::set_intersection, за да намери общите елементи и после ги сортира с функцията std::sort.
+// В main функцията се тества функцията като се подадат два прости масива arr1 и arr2.
+
+// void intersection(vector<int>& arr1, vector<int>& arr2) {
+//     vector<int> result;
+
+//     // set_intersection, за да намери общите елементи
+//     set_intersection(arr1.begin(), arr1.end(), arr2.begin(), arr2.end(), back_inserter(result));
+
+//     sort(result.begin(), result.end());
+
+//     for (int num : result) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main() {
+//     vector<int> arr1 = {1, 2, 3, 4, 5};
+//     vector<int> arr2 = {3, 4, 5, 6, 7};
+
+//     intersection(arr1, arr2);
+
+//     return 0;
+// }
 
 
 
 
+//  ------------------------------------------------------------------------------------------------------------
+
+// Task.9  Да се напише клас Movie, който има член-данни име, година на премиера и дали филмът има Оскар.
+
+// 9.1. Напишете функция, която извежда имената на всички филми, печелили Оскар;
+
+// 9.2. Напишете функция filter, която приема вектор от филми (с име Netflix например).
+// Нека тази функция извежда всички заглавия, излезли след 2010 година включително.
 
 
 
+
+// class Movie
+// {
+// private:
+
+//     string name;
+//     int premiereYear;
+//     bool hasOscar;
+
+// public:
+
+//     Movie(string name, int premiereYear, bool hasOscar)
+//     : name(name), premiereYear(premiereYear), hasOscar(hasOscar){}
+
+//     string getName(){return name;}
+//     int getPremiereYear(){return premiereYear;}
+//     bool hasWonOscar(){return hasOscar; }
+// };
+
+
+// void displayWinOscar(vector<Movie> movies)
+// {
+//     cout << "Movie win Oscar: " << endl;
+//     for (int i = 0; i < movies.size(); ++i) {
+
+
+//         if(movies[i].hasWonOscar()) {
+//             cout << movies[i].getName() << endl;;
+//         }
+
+//     }
+// }
+
+// void filter(vector<Movie> movies)
+// {
+//     cout << "Movie after 2010: " << endl;
+//     for (int i = 0; i < movies.size(); ++i) {
+
+
+//         if(movies[i].getPremiereYear() >= 2010 && movies[i].getName() == "Netflix") {
+//             cout << movies[i].getName() << endl;;
+//         }
+
+//     }
+// }
+
+// int main(){
+
+//     vector<Movie> movies =
+//     {
+//         Movie("The Godfather", 1972, true),
+//         Movie("Avatar", 2009, true),
+//         Movie("Star Wars: The Force Awakens", 2015, false),
+//         Movie("Inception", 2010, true),
+//         Movie("Toy Story", 1995, false),
+//         Movie("Home Alone", 1990, false),
+//         Movie("Interstellar", 2014, true),
+//         Movie("Netflix", 2014, false)
+//     };
+
+//     displayWinOscar(movies);
+//     cout << endl;
+//     filter(movies);
+
+//     return 0;
+// }
 
 
 
